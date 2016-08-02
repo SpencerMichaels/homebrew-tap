ccf8b333f78830a98a7c2830c478041311102142
Spencer Michaels <sxmichaels@gmail.com>
2016-08-01 20:15:36 -0500

    Fixed for brew audit

--- menubar-toggle.rb
+++ menubar-toggle.rb
@@ -1,9 +1,8 @@
 class MenubarToggle < Formula
-  desc "Toggle the OSX menu bar's autohide behavior from the command line."
+  desc "Toggle the OSX menubar's autohide setting from the command-line."
   homepage "https://github.com/sxmichaels/menubar-toggle"
   url "https://github.com/sxmichaels/menubar-toggle/archive/1.0.0.tar.gz"
   sha256 "4dafadd72f451e3c4bd996d890ecb4154dc99443bac19d127a8f5fd2563f7488"
-  version "1.0.0"
 
   def install
     system "./build"

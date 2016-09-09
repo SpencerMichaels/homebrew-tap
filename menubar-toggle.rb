09bd69a79e60004d5a4a317d7fa35909f3c4e3f7
Ben <BenjaminHCCarr@users.noreply.github.com>
2016-09-09 00:52:26 -0400

    Define work on EL Cap
    depends_on macos: '>= :el_capitan'
    
    single line edit

--- menubar-toggle.rb
+++ menubar-toggle.rb
@@ -4,6 +4,8 @@ class MenubarToggle < Formula
   url "https://github.com/sxmichaels/menubar-toggle/archive/1.0.0.tar.gz"
   sha256 "4dafadd72f451e3c4bd996d890ecb4154dc99443bac19d127a8f5fd2563f7488"
 
+  depends_on macos: '>= :el_capitan'
+
   def install
     system "./build"
     bin.install "menubar-toggle"

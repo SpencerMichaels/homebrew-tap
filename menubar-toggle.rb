ef24f276f73521d78e527cae06acf867816ae555
Spencer Michaels <sxmichaels@gmail.com>
2016-09-24 17:07:41 +0800

    added version limit

--- menubar-toggle.rb
+++ menubar-toggle.rb
@@ -4,6 +4,8 @@ class MenubarToggle < Formula
   url "https://github.com/sxmichaels/menubar-toggle/archive/2.0.0.tar.gz"
   sha256 "318c3d2122c3acde1afaf2570a3e7b938cc80bff4761ecc4177b1d659e18edf3"
 
+  depends_on :macos => :el_capitan
+
   def install
     system "swift build --configuration release"
     bin.install "./.build/release/menubar-toggle"
